d6e8b8765db5ed33ff85412e53f38450bcfb3d28
CristianJ77 <cristianfigueroa489@gmail.com>
2023-08-31 18:28:51 -0600

    carrito, login y registro agregados

--- server.js
+++ server.js
@@ -7,15 +7,29 @@ const PORT = process.env.PORT || 3000
 
 // Assets 
 app.use(express.static('public'))
-app.get('/', (req,res) => {
-    res.render('home')
-})
+
 
 //Template (vistas, colores, vistas)
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views' ))
 app.set('view engine', 'ejs')
 
+//Rutas
+app.get('/', (req,res) => {
+    res.render('home')
+})
+app.get('/cart', (req,res) => {
+    res.render('clientes/carrito')
+})
+app.get('/login', (req,res) => {
+    res.render('auth/login')
+})
+app.get('/register', (req,res) => {
+    res.render('auth/registro')
+})
+
+
+//Servidor localHost
 app.listen(PORT , () => {
     console.log(`Aplicación funcionando... ${PORT}`)
 })
